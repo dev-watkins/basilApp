3c5fd3ff3bae286639cf886d5f86c9f776d6b954
Matthew Watkins <ewx96@pm.me>
2022-12-05 22:33:07 -0600

    added logger

--- Nightshade/src/helpers/resolverWrapper.ts
+++ Nightshade/src/helpers/resolverWrapper.ts
@@ -2,11 +2,15 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { ZodError } from 'zod';
 import { GraphQLError } from 'graphql';
 import { REQUEST_VALIDATION_ERROR, DATABASE_REQUEST_ERROR } from '../framework';
+import { logger } from './logger';
 
-export const resolverWrapper = async (resolver: any) => {
+export const resolverWrapper = async (
+  resolver: () => Promise<any>
+): Promise<any> => {
   try {
     return await resolver();
   } catch (err) {
+    logger.error(err);
     if (err instanceof ZodError) {
       throw new GraphQLError('Request validation error', {
         extensions: {
